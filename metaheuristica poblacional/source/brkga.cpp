5e84eddac638a716312589157ae7bca008dcb7fa
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2025-11-02 17:59:35 -0300

    Co-authored-by: Dreykolin <Dreykolin@users.noreply.github.com> Co-authored-by: Joaquin San Martin Vargas <TheAmazingLife@users.noreply.github.com>

--- metaheuristica poblacional/source/brkga.cpp
+++ metaheuristica poblacional/source/brkga.cpp
@@ -1,16 +1,48 @@
+#include "brkga_class.h"
 #include "utils/GraphReader.h"
-#include "utils/brkga_mp_ipr/brkga_mp_ipr.hpp"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
 
+  /*
+  p = tamaño población
+  pe = población elite
+  pm = población mutante
+  rhoe = probabilidad de heredar del elite
+  */
+
   // Graph reading:
-  if (argc != 3 || std::string(argv[1]) != "-i") {
-    std::cerr << "Usage: <BRKGA> -i <problem-instance> -t tiempoMaximoSegundos\n";
+  if (argc != 13) {
+    std::cerr << "Uso: ./brkga -i <instancia.txt> -t <tiempoSegundos> "
+                 "-p <poblacion> -pe <elite> -pm <mutantes> -rhoe <herencia>\n";
     return 1;
   }
 
-  std::string filename = argv[2];
+  std::string filename;
+  double s = 0, pe = 0, pm = 0, rhoe = 0;
+  int p = 0;
+
+  // --- Leer argumentos ---
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    if (arg == "-i")
+      filename = argv[++i];
+    else if (arg == "-t")
+      s = std::stod(argv[++i]);
+    else if (arg == "-p")
+      p = std::stoi(argv[++i]);
+    else if (arg == "-pe")
+      pe = std::stod(argv[++i]);
+    else if (arg == "-pm")
+      pm = std::stod(argv[++i]);
+    else if (arg == "-rhoe")
+      rhoe = std::stod(argv[++i]);
+    else {
+      std::cerr << "Argumento desconocido: " << arg << "\n";
+      return 1;
+    }
+  }
+
   int V;
   std::vector<std::vector<int>> adj;
 
@@ -20,8 +52,14 @@ int main(int argc, char *argv[]) {
   }
 
   // --- BRKGA Algorithm ---
-  std::cout << "Testing sense: " << static_cast<int>(BRKGA::Sense::MINIMIZE)
-            << std::endl;
+  BRKGA brkga(V, p, pe, pm, rhoe, s, adj);
+  std::vector<int> independentSet = brkga.getSolution();
+
+  std::cout << independentSet.size() << "\n";
+  for (int i = 0; i < independentSet.size(); i++) {
+    std::cout << independentSet[i] << " ";
+  }
+  std::cout << "\n";
 
   return 0;
 }
@@ -35,16 +73,19 @@ pc : proporción de crossover (implícito)
 1. Se crea la población inicial con p individuos
 2. Bucle:
     1. el cromosoma pasa el decoder, el decoder da la solucion,
-    el fitness calcula el valor de la solucion, se guarda el fitness de cada inividuo
-    2. se ordena la poblacion segun su fitness: la elite(pe)  (ej: = 15%superior), y la no elite (pn), el resto.
-    3. se crea la nueva poblacion, con p individuos: Esta se llena un porcentaje con pe, con pm (mutantes) aleatorios
-        y pc (cruce).
-        3.1 el crossover selecciona un padre elite, un padre no elite, y con un porcentaje rho_e elige un gen de elite,
-            del resto del no elite, gen por gen.
+    el fitness calcula el valor de la solucion, se guarda el fitness de cada
+inividuo
+    2. se ordena la poblacion segun su fitness: la elite(pe)  (ej: =
+15%superior), y la no elite (pn), el resto.
+    3. se crea la nueva poblacion, con p individuos: Esta se llena un porcentaje
+con pe, con pm (mutantes) aleatorios y pc (cruce). 3.1 el crossover selecciona
+un padre elite, un padre no elite, y con un porcentaje rho_e elige un gen de
+elite, del resto del no elite, gen por gen.
     4. la poblacion anterior se descarta, la nueva poblacion es la creada.
     6. se repite bucle.
 3. Tras g generaciones, el algoritmo se detiene.
-4. La mejor solucion sera el individuo con mejor fitnes del grupo de elite de la ultima generacion.
+4. La mejor solucion sera el individuo con mejor fitnes del grupo de elite de la
+ultima generacion.
 
 
 
